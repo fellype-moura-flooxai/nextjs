d6b1f58935448cc02b76ddca37c38238aa878c51
fellype-moura-flooxai <fellype.moura.flooxai@gmail.com>
2025-04-23 13:55:35 -0300

    feat: fix errors and js/jsx integration global css and modularized css

---
+++ fundamentos/src/pages/index.jsx
@@ -0,0 +1,9 @@
+export default function Inicio() {
+    return (
+        <div>
+            <h1>fundamentos de next.js & react</h1>
+            <h2>vamos estudar esse framework</h2>
+        </div>
+    )
+
+}
--- fundamentos/src/pages/index.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <div>
-      <h1 className="text-red-500">Testee</h1>
-    </div>
-  );
-}
