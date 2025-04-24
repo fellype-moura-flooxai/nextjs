423a7311a4ac8db66865ba4a25509dcba8c2dc59
fellype-moura-flooxai <fellype.moura.flooxai@gmail.com>
2025-04-24 10:06:24 -0300

    feat: pre-rendering

--- fundamentos/src/pages/index.jsx
+++ fundamentos/src/pages/index.jsx
@@ -14,8 +14,10 @@ export default function Inicio() {
             <Navegador texto="JSX" destino="/jsx" cor="crimson" />
             <Navegador texto="Navegação #01" destino="/navegacao/" cor="green" />
             <Navegador texto="Navegação #02" destino="/cliente/sp-2/321" cor="blue" />
-            <Navegador texto="Componente com Estado" destino="/estado" cor="pink" />
-            <Navegador texto="Integração com API #01" destino="/integracao_1" cor="yellow" />
+            <Navegador texto="Componente com Estado" destino="/estado" cor="#a45b71" />
+            <Navegador texto="Integração com API #01" destino="/integracao_1" cor="#42a9a9" />
+            <Navegador texto="Conteudo Estatico" destino="/estatico" cor="#fa054a" />
+            <Navegador texto="Conteudo dinamico" destino="/dinamico" cor="black" />
         </div>
     )
 
